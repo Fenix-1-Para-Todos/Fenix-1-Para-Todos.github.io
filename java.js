2f683a605dec0bb34431406d923b21f6ed95b4d6
Fenix-1-Para-Todos <125103961+Fenix-1-Para-Todos@users.noreply.github.com>
2023-02-10 18:51:48 -0700

    Initial commit

---
+++ java.js
@@ -0,0 +1,88 @@
+uno.addEventListener('click',() =>{
+    let sound = new Audio('1.mp3');
+    sound.play();
+})
+dos.addEventListener('click',() =>{
+    let sound = new Audio('2.mp3');
+    sound.play();
+})
+tres.addEventListener('click',()=>{
+    let sound = new Audio('3.mp3');
+    sound.play();
+})
+cuatro.addEventListener('click',()=>{
+    let sound = new Audio('4.mp3');
+    sound.play();
+})
+cinco.addEventListener('click',()=>{
+    let sound = new Audio('5.mp3');
+    sound.play();
+})
+seis.addEventListener('click',()=>{
+    let sound = new Audio('6.mp3');
+    sound.play();
+})
+siete.addEventListener('click',()=>{
+    let sound = new Audio('7.mp3');
+    sound.play();
+})
+ocho.addEventListener('click',()=>{
+    let sound = new Audio('8.mp3');
+    sound.play();
+})
+nueve.addEventListener('click',()=>{
+    let sound = new Audio('9.mp3');
+    sound.play();
+})
+cero.addEventListener('click',()=>{
+    let sound = new Audio('0.mp3');
+    sound.play();
+})
+espejo.addEventListener('click',()=>{
+    let sound = new Audio('Espejo.mp3');
+    sound.play();
+})
+regreso.addEventListener('click',()=>{
+    let sound = new Audio('Regreso.mp3');
+    sound.play();
+})
+cambiodecolor.addEventListener('click',()=>{
+    let sound = new Audio('CAMBIO.mp3');
+    sound.play();
+})
+mascuatro.addEventListener('click',()=>{
+    let sound = new Audio('+4.mp3');
+    sound.play();
+})
+bloqueomascuatro.addEventListener('click',()=>{
+    let sound = new Audio('No4.mp3');
+    sound.play();
+})
+masdos.addEventListener('click',()=>{
+    let sound = new Audio('+2.mp3');
+    sound.play();
+})
+paso.addEventListener('click',()=>{
+    let sound = new Audio('paso.mp3');
+    sound.play();
+})
+morado.addEventListener('click',()=>{
+    let sound = new Audio('Morado.mp3');
+    sound.play();
+})
+turquesa.addEventListener('click',()=>{
+    let sound = new Audio('Turquesa.mp3');
+    sound.play();
+})
+naranja.addEventListener('click',()=>{
+    let sound = new Audio('Naranja.mp3');
+    sound.play();
+})
+rosa.addEventListener('click',()=>{
+let sound= new Audio('Rosa.mp3');
+sound.play();
+})
+Bloqueo.addEventListener('click',() =>{
+    let sound = new Audio('Bloqueo1.mp3');
+    sound.play();
+})
